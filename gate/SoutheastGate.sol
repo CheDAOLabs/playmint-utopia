729b5417eaf791f645299cb7a4562c0431166cea
Frost <froststarbook@gmail.com>
2024-05-13 20:13:01 +0800

    add gate

---
+++ gate/SoutheastGate.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {State} from "cog/IState.sol";
+import {Schema} from "@ds/schema/Schema.sol";
+import {Gate} from "./Gate.sol";
+
+using Schema for State;
+
+contract SquishyGate is Gate {
+    function getKeyId() internal pure override returns (bytes24) {
+        return 0x6a7a67f0938bd985000000000000000c0000000c0000000c;
+    }
+}
